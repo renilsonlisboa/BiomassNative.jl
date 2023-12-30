b178426239a449f54b3fede4b5a80c76b832455f
Renilson Lisboa Júnior <renilsonlisboajunior@gmail.com>
2023-12-29 21:06:54 -0300

    new file:   src/Anotation.jl 	modified:   src/BiomassNative.jl 	new file:   src/ImportData.jl 	new file:   src/Save.jl

--- src/BiomassNative.jl
+++ src/BiomassNative.jl
@@ -1,5 +1,40 @@
 module BiomassNative
 
-# Write your package code here.
+# Inclui os módulos auxiliares no projeto
+include(joinpath(@__DIR__, "src/Save.jl"))
+include(joinpath(@__DIR__, "src/ImportData.jl"))
+
+import QML: QString, @qmlfunction, loadqml, exec
+
+# Exporta a função Inventory possibilitando ser chamada via terminal pelo usuário
+export Inventory
+
+    # Função para seleção de arquivo em .CSV com dados para processamento
+    function singleFile(arg)
+        ImportData.singlefile(arg)
+    end
+
+    # Função salvar os resultados do processamento em .XLSX
+    function saveFile(Resultado, uri)
+        Save.saveFile(Resultado, uri)
+    end
+
+    # processamento do Inventário por meio da amostragem aleatória simples
+    function calcAAS(Dados, Area, AreaParc, α, EAR)
+        AAS.calcAAS(Dados, Area, AreaParc, α, EAR)
+    end
+
+    # Ativa o programa em QML
+    function Inventory()
+        
+        @qmlfunction singleFile saveFile 
+
+        current_directory = dirname(@__FILE__)
+
+        loadqml(joinpath(current_directory, "src/qml", "main.qml"))
+
+        exec()
+
+    end
 
 end
